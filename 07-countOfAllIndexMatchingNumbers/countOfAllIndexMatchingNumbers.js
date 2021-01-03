f1e9330619e682e1ee697c35ec2da4636a6870e8
Ruvini Senevirathna <ruviniruvi@gmail.com>
2021-01-02 23:16:26 -0500

    solution for problem 08

--- 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -1,5 +1,6 @@
 function countOfAllIndexMatchingNumbers(nums) {
   // Insert code here;
+  return nums.filter(items => items === nums  ).length;
 }
 
 // Do not edit this line;
--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,5 +1,12 @@
 function sumOfAllEvenNumbers(nums) {
   // Insert code here;
+   var sum = 0;
+  for ( var i = 0; i <= nums.length; i++) {
+    if (nums[i] % 2 === 0) {
+    sum ++;
+    }
+  }
+  return sum;
 }
 
 // Do not edit this line;
