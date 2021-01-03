e240452d5384c30b4b2add63f4135ad47739e0d8
Ruvini Senevirathna <ruviniruvi@gmail.com>
2021-01-03 09:06:57 -0500

    solution for problem 17

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -2,9 +2,8 @@ function productOfAnyAmountOfNumbers(...args) {
   // Insert code here;
 
   var total   =   1;
-  
   for(var i of args){
-         
+      
            total   *=  i;
        }
      return total;
--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,19 @@
 function setUnionOfAnyAmountOfSets(...args) {
   // Insert code here;
+  var newSet =new Set();
+  for(var i of args )
+  {  
+    for(var j of i )
+    {
+      if(newSet!=args[j]) 
+      newSet.add(j);
+    }
+   
+  }
+  return newSet;
+
+
+
 }
 
 // Do not edit this line;
