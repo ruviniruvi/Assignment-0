8253706a29129222dcc0aa29c2d67854f0e12176
Ruvini Senevirathna <ruviniruvi@gmail.com>
2021-01-03 08:48:34 -0500

    solution for problem 16

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,6 +1,18 @@
 function productOfAnyAmountOfNumbers(...args) {
   // Insert code here;
+
+  var total   =   1;
+  
+  for(var i of args){
+         
+           total   *=  i;
+       }
+     return total;
 }
+
+
+
+
 
 // Do not edit this line;
 module.exports = productOfAnyAmountOfNumbers;
