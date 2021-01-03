ee7f1944d13922e155f57bd46a4ee7ed23f45b23
Ruvini Senevirathna <ruviniruvi@gmail.com>
2021-01-03 00:10:58 -0500

    solution for problem 14

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,14 @@
 function frequencyCounter(word) {
   // Insert code here;
+  var result={};
+  for(let i = 0; i <word.length; i++){
+    if(result[word[i]] ){
+      result [word[i]]++;
+    }else{
+      result[word[i]]=1;
+    }
+  }
+return result;
 }
 
 // Do not edit this line;
