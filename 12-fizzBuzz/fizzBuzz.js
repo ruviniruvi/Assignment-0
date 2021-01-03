b65d8905bb596335afe477025f01580e4f8b7eaa
Ruvini Senevirathna <ruviniruvi@gmail.com>
2021-01-02 23:44:55 -0500

    solution for problem 12

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,18 @@
 function fizzBuzz(start, end) {
   // Insert code here;
+  var result=[];
+  for(let i = start; i <= end; i++){
+    if(i % 15 === 0 ){
+      result.push("FizzBuzz");
+    }else if (i % 3 === 0){
+      result.push("Fizz");
+    }else if (i % 5 === 0){
+      result.push("Buzz");
+    }else{
+      result.push(i);
+    }
+  }
+return result;
 }
 
 // Do not edit this line;
