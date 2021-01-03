aa70a613f70882d4b61f61e080fde283bc5fa3c1
Ruvini Senevirathna <ruviniruvi@gmail.com>
2021-01-03 11:03:27 -0500

    solution for problem 19

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -5,7 +5,35 @@ class MySolution {
 
   binarySearch(nums, target) {
     // Insert code here;
-  }
+    //let start = 0;
+   // let end = nums.length - 1;
+
+    if(!nums.length)
+    {
+      return false;
+    }
+    else{
+        let middle = Math.floor(nums.length / 2);
+
+        if (nums[middle] === target) {
+            // found the target
+            return true;
+        } 
+        else {
+          if (nums[middle] < target)
+           {
+            // continue searching to the right
+            return this.binarySearch(nums.slice(middle + 1),target);
+        } 
+        else if(nums[middle]>target){
+            // search searching to the left
+            return this.binarySearch(nums.slice(0, middle ),target);}
+        }
+    
+      }
+
+  
+      }
 }
 
 // Do not edit this line;
