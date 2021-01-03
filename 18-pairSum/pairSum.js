3d50b081c702b8dfbcfc79a0067667d69f3e351d
Ruvini Senevirathna <ruviniruvi@gmail.com>
2021-01-03 10:07:14 -0500

    solution for problem 18

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,26 @@
 function pairSum(nums, target) {
   // Insert code here;
+
+var newSet = [];
+if (nums.length <= 1)
+    {
+      throw("The array length is not enough.");
+    }
+
+  for (var i = 0; i < nums.length; i++) {
+    for (var j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        newSet.push(i);
+        return true;
+       // newSet.push(j);
+      }
+    }
+  }
+  return false;
+
+
+
+
 }
 
 // Do not edit this line;
