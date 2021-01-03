76a79290c4b89b2e73a190dbaec83e159bc83951
Ruvini Senevirathna <ruviniruvi@gmail.com>
2021-01-03 07:55:50 -0500

    solution for problem 04

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,7 +1,12 @@
 function countOfAllBooleans(arr) {
   // Insert code here;
-  var count = arr.filter(Boolean).length;
-  return count;
+  var i=0;
+  for (objects in arr) {
+    if (arr[objects] === true || arr[objects]===false) {
+       i++;
+    }
+}
+return i;
 }
 
 // Do not edit this line;
