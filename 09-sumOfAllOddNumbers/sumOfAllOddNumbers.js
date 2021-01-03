edc94518529ecb03d672ed193bb19f0a44c6cd0a
Ruvini Senevirathna <ruviniruvi@gmail.com>
2021-01-02 23:20:36 -0500

    solution for problem 09

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,5 +1,13 @@
 function sumOfAllOddNumbers(nums) {
   // Insert code here;
+  var sum = 0;
+  for ( var i = 0; i <= nums.length; i++) {
+    if (nums[i] % 2 === 1) {
+    sum ++;
+    }
+  }
+  return sum;
+
 }
 
 // Do not edit this line;
