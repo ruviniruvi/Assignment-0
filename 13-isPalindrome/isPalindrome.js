97cd00b54cf90ddb5150cc89f9067c44f95aae5f
Ruvini Senevirathna <ruviniruvi@gmail.com>
2021-01-02 23:53:28 -0500

    solution for problem 13

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,6 +1,16 @@
 function isPalindrome(word) {
   // Insert code here;
-}
 
+  var re = /[^A-Za-z0-9]/g; 
+ word = word.toLowerCase().replace(re, '');
+
+
+ var len = word.length; 
+ 
+ for (var i = 0; i < len/2; i++) {
+   if (word[i] !== word[len - 1 - i]) { return false; }
+   return true;
+}
+}
 // Do not edit this line;
 module.exports = isPalindrome;
